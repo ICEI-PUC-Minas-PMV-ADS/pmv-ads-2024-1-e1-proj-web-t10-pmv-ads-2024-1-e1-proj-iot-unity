6a2a984d3e392efd8cb7f5687a0990036949531b
Joel-Onofre <joel.jose@sga.pucminas.br>
2024-06-04 22:16:59 -0300

    Update funcionalidade
    Funcionalidades de adicionar, editar e remover dispositivo adicionadas juntamente ao local storage

--- codigo-fonte/src/Dashboard/funcoes.js
+++ codigo-fonte/src/Dashboard/funcoes.js
@@ -24,10 +24,18 @@ function montaTabela() {
       `;
   });
   conteudoDispositivos.innerHTML = conteudo;
+
+  // determina comportamento dos botões e outros componentes interativos
+  let botoesSelecao = document.querySelectorAll("input[name=campoSelecao]");
+  botoesSelecao.forEach((b) => {
+    b.onclick = function (e) {
+      btMEditar.disabled = false;
+      btMRemover.disabled = false;
+    };
+  });
+  btMEditar.disabled = true;
+  btMRemover.disabled = true;
 }
-
-
-
 
 // Mostra a janela modal para adição de novo dispositivo
 document.getElementById("addDispositivo").onclick = function () {
@@ -40,57 +48,129 @@ document.getElementById("addDispositivo").onclick = function () {
     modalDispositivo.style.display = "block";
     btMTCriar.style.display = "inline-block";
     btMTCriar.disabled = true;
+    btMAlterar.style.display = "none";
+    btMExcluir.style.display = "none";
+    btMAlterar.disabled = true;
     campoDescricao.focus();
   };
   
-  // Confirma a criação do dispositivo
-  btMTCriar.onclick = function () {
-    let dispositivo = {
-      nome: campoNome.value,
-      descricao: campoDescricao.value,
-      localizacao: campoLocalizacao.value,
-    };
-    modalDispositivo.style.display = "none";
-    createDispositivo(dispositivo);
+// Confirma a criação do dispositivo
+btMTCriar.onclick = function () {
+  let dispositivo = {
+    nome: campoNome.value,
+    descricao: campoDescricao.value,
+    localizacao: campoLocalizacao.value,
   };
-  
-  // Configura o botão de fechar a janela modal
-  fechaModal.onclick = function () {
-    modalDispositivo.style.display = "none";
-  };
+  modalDispositivo.style.display = "none";
+  btMExcluir.style.display = "inline-block";
+  createDispositivo(dispositivo);
+};
 
-  // Configura o botão de fechar a janela da lista de dispositivos
-  fechaModal2.onclick = function () {
-    modalDispositivos.style.display = "none";
-  };
-
-  // Cancela a criação do dispositivo
-btMTCancelar.onclick = function () {
-    modalDispositivo.style.display = "none";
-  };
-  
-  // Verifica se os três campos estão preenchidos antes de criar o dispositivo
-  let liberaBotaoMT = function () {
+// Verifica se os três campos estão preenchidos antes de criar o dispositivo
+let liberaBotaoMT = function () {
     if (
       campoNome.value.length > 0 &&
       campoDescricao.value.length > 0 &&
       campoLocalizacao.value.length > 0
     ) {
       btMTCriar.disabled = false;
-      btMTAlterar.disabled = false;
+      btMAlterar.disabled = false;
     } else {
       btMTCriar.disabled = true;
-      btMTAlterar.disabled = true;
+      btMAlterar.disabled = true;
     }
   };
   campoNome.onchange = liberaBotaoMT;
   campoDescricao.onchange = liberaBotaoMT;
   campoLocalizacao.onchange = liberaBotaoMT;
 
-  document.getElementById("listaDispositivos").onclick = function () {
-    montaTabela()
-    modalDispositivos.style.display = "block"
+// Configura o botão de fechar a janela modal
+fechaModal.onclick = function () {
+  modalDispositivo.style.display = "none";
+};
+
+// Cancela a criação do dispositivo
+btMTCancelar.onclick = function () {
+  modalDispositivo.style.display = "none";
+};
+
+// Mostra a tabela de dispositivos
+document.getElementById("listaDispositivos").onclick = function () {
+  montaTabela()
+  modalDispositivos.style.display = "block"
+  btMExcluir.style.display = "inline-block";
+};
+// Configura o botão de fechar a janela da lista de dispositivos
+fechaModal2.onclick = function () {
+  modalDispositivos.style.display = "none";
+};
+
+// Configura o botão Editar para abrir o modal
+btMEditar.onclick = function () {
+  let selecoes = Array.from(
+    document.querySelectorAll("input[name=campoSelecao]")
+  );
+  let selecionado = selecoes.find((i) => i.checked == true);
+  if(selecionado) {
+    let dispositivo = readDispositivo(selecionado.value);
+    campoNome.value = dispositivo.nome;
+    campoDescricao.value = dispositivo.descricao;
+    campoLocalizacao.value = dispositivo.localizacao;
+    campoID.value = dispositivo.id
+    campoNome.disabled = false;
+    campoDescricao.disabled = false;
+    campoLocalizacao.disabled = false;
+    modalDispositivos.style.display = "none"
+    modalDispositivo.style.display = "block";
+    btMTCriar.style.display = "none";
+    btMAlterar.style.display = "inline-block";
+    btMExcluir.style.display = "none";
+    btMAlterar.disabled = false;
+    campoNome.focus();
   }
+};
+
+// Confirma a alteração do dispositivo
+btMAlterar.onclick = function () {
+  let dispositivoAtualizado = {
+    id: parseInt(campoID.value),
+    nome: campoNome.value,
+    descricao: campoDescricao.value,
+    localizacao: campoLocalizacao.value,
+  };
+  updateDispositivo(dispositivoAtualizado);
+  modalDispositivo.style.display = "none";
+  montaTabela();
+};
+
+btMRemover.onclick = function () {
+  let selecoes = Array.from(
+    document.querySelectorAll("input[name=campoSelecao]")
+  );
+  let selecionado = selecoes.find((i) => i.checked == true);
+  if(selecionado) {
+    let dispositivo = readDispositivo(selecionado.value);
+    campoNome.value = dispositivo.nome;
+    campoDescricao.value = dispositivo.descricao;
+    campoLocalizacao.value = dispositivo.localizacao;
+    campoID.value = dispositivo.id
+    campoNome.disabled = true;
+    campoDescricao.disabled = true;
+    campoLocalizacao.disabled = true;
+    modalDispositivos.style.display = "none"
+    modalDispositivo.style.display = "block";
+    btMTCriar.style.display = "none";
+    btMAlterar.style.display = "none";
+    btMExcluir.style.display = "inline-block";
+  }
+};
+
+// Confirma a exclusão do dispositivo
+btMExcluir.onclick = function () {
+  deleteDispositivo(parseInt(campoID.value));
+  modalDispositivo.style.display = "none"
+  montaTabela();
+};
 
   montaTabela();
 
